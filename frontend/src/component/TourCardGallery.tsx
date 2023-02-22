c69d3253a76d74eb4cdc25b70a2213ee149d0a46
TG Frerich <themuppet.coder@gmail.com>
2023-02-22 17:55:00 +0100

    added TourCard and TourCardGallery

---
+++ frontend/src/component/TourCardGallery.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TourCard from "./TourCard";
+import {Tour} from "../model/Tour";
+
+type TourCardGalleryProps = {
+    tourList: Tour[]
+}
+
+
+export default function TourCardGallery(props: TourCardGalleryProps) {
+
+
+
+}
