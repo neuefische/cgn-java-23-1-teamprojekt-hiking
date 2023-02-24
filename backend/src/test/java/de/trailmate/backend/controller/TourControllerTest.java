1f0bf549b2e5bc8c5554791bf09ff1b940eb9ce9
daniel <daniel.windel@unitybox.de>
2023-02-24 12:46:45 +0100

    added integration tests for add Tour Method

--- backend/src/test/java/de/trailmate/backend/controller/TourControllerTest.java
+++ backend/src/test/java/de/trailmate/backend/controller/TourControllerTest.java
@@ -4,6 +4,7 @@ package de.trailmate.backend.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.trailmate.backend.model.Tour;
 import de.trailmate.backend.repository.TourRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
