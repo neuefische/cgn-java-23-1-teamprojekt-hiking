93e57f777abb78ee97bd41b1dd59f5a86e5cce14
TG Frerich <themuppet.coder@gmail.com>
2023-02-24 17:46:50 +0100

    started to implement Add method

---
+++ frontend/src/component/AddTour.tsx
@@ -0,0 +1,44 @@
+import React, {FormEvent, useState} from "react";
+import {Tour} from "../model/Tour";
+import "../Styling/AddTour.css"
+
+
+export default function AddTour() {
+
+    const [state, setState] = React.useState({
+        title: "",
+        description: "",
+        category: ""
+    })
+
+
+    const handleSubmit =  (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(state)
+    }
+
+    function handleChange(evt: any) {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        })
+    console.log(state)
+    };
+
+    return(
+        <div>
+                <form onSubmit={handleSubmit} >
+                    <h1>share Tour - share Moments</h1>
+                    <label>Title</label>
+                    <input type="text" value={state.title} onChange={handleChange} name="title" />
+                    <label>Category</label>
+                    <input type="text" value={state.category} onChange={handleChange} name="category" />
+                    <label>Description</label>
+                    <input type="text" value={state.description} onChange={handleChange} name="Description" />
+                    <button>share your Moment</button>
+                </form>
+        </div>
+    )
+
+}
