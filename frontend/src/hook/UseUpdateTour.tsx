4c428cb4df78e377e665f50fb09d936924c68c67
timlenny <schlotter.tim@gmail.com>
2023-03-03 11:03:30 +0100

    Implemented Update MTH in Back-&Frontend

---
+++ frontend/src/hook/UseUpdateTour.tsx
@@ -0,0 +1,29 @@
+import {Tour} from "../model/Tour";
+import axios from "axios";
+
+export default function useUpdateTour(){
+
+    function updateSingleTour(props: Tour | undefined){
+
+        if(props) {
+
+            axios.put("/api/tours/" + props.id, {
+                title: props.title,
+                description: props.description,
+                startLongitude: props.startLongitude,
+                startLatitude: props.startLatitude,
+                endLongitude: props.endLongitude,
+                endLatitude: props.endLatitude,
+                category: props.category
+            })
+                .then((response) => {
+                    console.log(props)
+                    window.location.assign("/tours")
+                })
+                .catch((error) => console.error(error))
+        }
+
+    }
+
+ return {updateSingleTour}
+}
