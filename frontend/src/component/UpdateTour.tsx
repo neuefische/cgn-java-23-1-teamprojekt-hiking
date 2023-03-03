6b98ad00a80f7f8f51bd251e6ceee44a4bfa9cd2
timlenny <schlotter.tim@gmail.com>
2023-03-03 11:03:05 +0100

    Implemented Update MTH in Back-&Frontend

--- frontend/src/component/UpdateTour.tsx
+++ frontend/src/component/UpdateTour.tsx
@@ -1,8 +1,15 @@
-import React, {useState} from "react";
-import AddSingleTour from "../hook/AddSingleTour";
+import React, {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import {Tour} from "../model/Tour";
 import {useParams} from "react-router-dom";
 import GetTours from "../hook/GetTours";
+
+
+
+const myAsynFunction = async (id: String | undefined) => {
+    const allTours = await GetTours();
+    const tourToUpdate:Tour[] = await allTours.filter((tour) => { return tour.id === id }).map((result:Tour) => { return result  })
+    return tourToUpdate;
+}
 
 
 export default function UpdateTour() {
@@ -10,18 +17,31 @@ export default function UpdateTour() {
     const params = useParams();
     const id: string | undefined = params.id;
 
-    const tours = GetTours();
-    //const tourToUpdate = tours.filter(value => {return value.id === id}).map((tour:Tour) =>resultTour:Tour = {id: tour.id, title: tour.title } )
+    const {updateSingleTour} = useUpdateTour();
+    const allTours = GetTours();
+    const [addTour, setAddTour] = useState<Tour | undefined>();
 
-    const {postSingleTour} = AddSingleTour()
 
-    const [addTour, setAddTour] = useState<Tour>();
+    //const tourToUpdate:Tour[] = allTours.filter((tour) => { return tour.id === id }).map((result:Tour) => { return result  })
 
-    /* /!* const [inputFields, setInputFields] = React.useState({
-          //title: tourToUpdate.title,
-          //description: tourToUpdate.description,
-          category: tourToUpdate.category
-      })*!/
+    const [inputFields, setInputFields] = useState({
+        title: "",
+        description: "",
+        category: "",
+    });
+
+
+    useEffect(() => {
+        const tour = allTours.find((tour) => tour.id === id);
+        if (tour) {
+            setAddTour(tour);
+            setInputFields({
+                title: tour.title,
+                description: tour.description,
+                category: tour.category,
+            });
+        }
+    }, [id, allTours]);
 
 
       const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -33,10 +53,10 @@ export default function UpdateTour() {
       function handleChange(evt: ChangeEvent<HTMLInputElement>) {
           setInputFields({...inputFields, [evt.target.name]: evt.target.value})
           setAddTour({
-              title: inputFields.title,
-              description: inputFields.description,
-              category: inputFields.category,
-              id: inputFields.id,
+              title: inputFields.title.toString(),
+              description: inputFields.description.toString(),
+              category: inputFields.category.toString(),
+              id: "",
               endLatitude: 12.123,
               endLongitude: 12.123,
               startLatitude: 12.123,
@@ -53,7 +73,7 @@ export default function UpdateTour() {
               <input type="text" value={inputFields.category} onChange={handleChange} name="category"/>
               <label>Description</label>
               <input type="text" value={inputFields.description} onChange={handleChange} name="description"/>
-              <button onClick={() => handleChange}>share your Moment</button>
+              <button onClick={() => handleChange}>Share your Moment</button>
           </form>
-      )*/
+      )
 }
