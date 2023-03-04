850e59a2b3a07fcc0fcac8f861ebda1532ef6121
Hendrik Neuschulz <hneuhb@t-online.de>
2023-03-04 17:57:03 +0100

    first try to add some styling to the cards

--- frontend/src/component/TourCardDetails.tsx
+++ frontend/src/component/TourCardDetails.tsx
@@ -2,6 +2,7 @@ import {Tour} from "../model/Tour";
 import {Link, useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import testtwo from "../testtwo.jpg";
 
 
 export default function TourCardDetails (){
@@ -29,15 +30,25 @@ export default function TourCardDetails (){
     }
 
     return(
-        <div>
-            <p>{details.title}</p>
-            <p>{details.category}</p>
-            <p>{details.description}</p>
-            <p>Start:{details.startLongitude} {details.startLatitude}</p>
-            <p>End: {details.endLongitude}  {details.endLatitude}</p>
-            <Link to={"/tours/edit/" + id}>Edit</Link>
-            
+        <div className="TourCard">
+            <div className="cards">
+                <div className="card-item">
+                    <div className="card-image">
+                        <img src={testtwo}  height={"200"} width={"100%"} alt={"TourCard Image"}/>
+                    </div>
+                    <div className="card-info">
+                        <p>{details.title}</p>
+                        <p>{details.category}</p>
+                        <p>{details.description}</p>
+                        <p>Start:{details.startLongitude} {details.startLatitude}</p>
+                        <p>End: {details.endLongitude}  {details.endLatitude}</p>
+                        <Link to={"/tours/edit/" + id}>Edit</Link>
+
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 
 }
