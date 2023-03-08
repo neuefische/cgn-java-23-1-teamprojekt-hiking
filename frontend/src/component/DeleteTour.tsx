7b125179379d5c29e6b7bd1cfc48968a686142f8
Hendrik Neuschulz <hneuhb@t-online.de>
2023-03-08 16:30:58 +0100

    create branch für US8 and add delete Method

---
+++ frontend/src/component/DeleteTour.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default function DeleteTour(id: string){
+
+}
+
+
+
+
+
+
+
+
+
+/*
+axios.delete("/api/tours/${id}").then((response) => {
+    console.log("Tour with ID ${id} deleted successfully");
+}).catch((error) => {
+    console.error("deleting not successfully")
+})*/
